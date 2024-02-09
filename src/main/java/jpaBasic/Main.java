68f70eedba5e8125abc84e5553ff486fcfbdd2e6
전성호 <kopasd99@gmail.com>
2024-02-09 20:21:02 +0800

    add proxy

--- src/main/java/jpaBasic/Main.java
+++ src/main/java/jpaBasic/Main.java
@@ -16,17 +16,6 @@ public class Main {
         tx.begin();
 
         try{
-            Movie movie=new Movie();
-            movie.setDirector("SungHo");
-            movie.setActor("BBB");
-            movie.setName("바람과 함꼐 사라지다");
-            movie.setPrice(10000);
-
-            em.flush();
-            em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         }catch(Exception e){
